0280049dd6bf2ba731d44cf1580ad36fd651d012
Om Ra <me@xomrkob.ru>
2026-09-10 12:59:47 +0600

    refactor(database): drop GORM AutoMigrate; schema owned by db-migrate

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,11 +2,9 @@ package database
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/mrhumster/identity-service/config"
-	"github.com/mrhumster/identity-service/internal/domain/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -26,8 +24,5 @@ func SetupDatabase(cfg *config.Config) *gorm.DB {
 	sqlDb.SetMaxIdleConns(10)
 	sqlDb.SetConnMaxLifetime(time.Hour)
 	sqlDb.SetConnMaxIdleTime(30 * time.Minute)
-	log.Printf("🔌  Creating uuid-ossp extension...")
-	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
-	db.AutoMigrate(&models.User{})
 	return db
 }
